ddeca4e2136421c969608d41f53605a630282d2a
Ice_Pana007 <kolm.v.a.2005@gmail.com>
2024-02-04 16:12:59 +0800

    подготовка рабочего пространства

---
+++ main/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import context
+
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'Главная страница Магазина - HOME'
+    }
+
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return HttpResponse("About page")
