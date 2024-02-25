9bf04074ad658be92b6ca5aa09eaaafb4ec63ca9
IcePanda007 <kolm.v.a.2005@gmail.com>
2024-02-25 16:00:42 +0800

    вопросы в поле name путь fixtures/goods/cats.json

--- goods/models.py
+++ goods/models.py
@@ -1,12 +1,18 @@
+from tabnanny import verbose
 from django.db import models
 
 class Categories(models.Model):
-    name =models.CharField(max_length=150, unique=True)
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
+    name =models.CharField(max_length=150, unique=True, verbose_name='название')
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
 
     class Meta:
         db_table = 'category'
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
 
+
+    def __str__(self):
+        return self.name
 
 class Products(models.Model):
     name =models.CharField(max_length=150, unique=True, verbose_name='Название')
@@ -17,3 +23,13 @@ class Products(models.Model):
     discount = models.DecimalField(default=0.00,max_digits=7,decimal_places=2, verbose_name='Скидка в %')
     quantity = models.PositiveIntegerField(default=0, verbose_name='Количество')
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
+
+
+class Meta:
+    db_table = 'product'
+    verbose_name = 'Продукт'
+    verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return self.name
+
